37bf8bc1e177b910433a69ca737ca6b9f80590c5
Chinmay-k8 <workchinmaynov29@gmail.com>
2024-08-29 12:48:40 +0530

    Added the i/o field in form

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('range-name/{id}', [MasterController::class, 'getRangeNameById']);
 Route::get('section-name/{id}', [MasterController::class, 'getSectionNameById']);
 Route::get('beat-name/{id}', [MasterController::class, 'getBeatNameById']);
 Route::get('forestblock-name/{id}', [MasterController::class, 'getForestblockNameById']);
-// Route::get('/list-data', [ListController::class, 'fetchData'])->name('list.data');
+Route::get('/list-data', [ListController::class, 'fetchData'])->name('list.data');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/form', [FormController::class, 'showForm'])->name('form.show');
@@ -45,7 +45,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/excel', [ExcelController::class, 'showForm'])->name('excel.show');
     Route::post('/excel/upload', [ExcelController::class, 'upload'])->name('excel.upload');
     Route::get('/list', [ListController::class, 'showList'])->name('list.show');
-    Route::get('/list-data', [ListController::class, 'fetchData'])->name('list.data');
+    // Route::get('/list-data', [ListController::class, 'fetchData'])->name('list.data');
 
 
 });
