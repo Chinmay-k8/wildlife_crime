45e0f72bcbb51a1a0f3591adebbcdb2dcb66fa1d
Chinmay Kar <workchinmaynov29@gmail.com>
2024-09-11 17:59:44 +0530

    Fixed listing ui

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -38,8 +38,8 @@ td {
 </style>
 
 
-<div class="container mt-5">
-    <div class="col-30">
+<div class="row">
+    <div class="col-12">
         <div class="card">
             <div class="card-body">
                 <div class="table-responsive">
